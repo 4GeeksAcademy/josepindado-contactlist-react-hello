a6bf3eabc615703875bb74adc22e66ba65c31a4b
Jose Pindado <joseangelpindado@gmail.com>
2024-11-18 19:27:07 +0000

    Revert "Final commit"
    This reverts commit 23e35d97b24ac35a0ac3e2a65b46542cbcdc2dfd.

--- src/js/layout.js
+++ src/js/layout.js
@@ -11,11 +11,15 @@ import injectContext from "./store/appContext";
 import AddNewContact from "./views/addnewcontact";
 import UpdateContact from "./views/updatecontact"
 
+//create your first component
 const Layout = () => {
+	//the basename is used when your project is published in a subdirectory and not in the root of the domain
+	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+	const basename = process.env.BASENAME || "";
 
 	return (
 		<div>
-			<BrowserRouter>
+			<BrowserRouter basename={basename}>
 				<ScrollToTop>
 					<Routes>
 						<Route path="/" element={<Home />} />
